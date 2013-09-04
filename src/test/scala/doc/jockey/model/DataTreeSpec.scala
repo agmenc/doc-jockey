7e14ff0ecf959ddaa74304f5b9c2804b0929f320
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2013-09-04 08:01:07 +0100

    Lay out some data tree specs

---
+++ src/test/scala/doc/jockey/model/DataTreeSpec.scala
@@ -0,0 +1,24 @@
+package doc.jockey.model
+
+import org.scalatest.WordSpec
+
+class DataTreeSpec extends WordSpec {
+  "We can build a tree of data to represent a test" in {
+    new Command{
+      def execute = ???
+    }
+  }
+
+  // TODO - CAS - 04/09/2013 - Zippers, anyone?
+  "We can map the data tree to an executed data tree" in {
+    assert(true === false)
+  }
+
+  "We can map an executed data tree to HTML" in {
+    assert(true === false)
+  }
+
+  "We can map any data tree to HTML" in {
+    assert(true === false)
+  }
+}
