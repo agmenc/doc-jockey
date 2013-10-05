90c04c79afee226735fede8de65bb9c72be93c50
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2013-10-05 21:38:12 +0100

    Remove redundant tests

--- src/test/scala/doc/jockey/rendering/TreePainterSpec.scala
+++ src/test/scala/doc/jockey/rendering/TreePainterSpec.scala
@@ -12,35 +12,16 @@ class TreePainterSpec extends WordSpec with TableAware {
     def actuals = List("Does this thing work?", testPasses)
   }
 
-  "We can paint a single Before" in {
+  "Befores render from expecteds, while Afters render from actual Results" in {
     val cmd = SingleRowCmd(true)
-    val test = Before(cmd) :: Nil
+    val before = Before(cmd)
+    val after = before.execute
 
-    val result = new TreePainter().paint(test)
-
-    assert(result.tables.head.cells === title :: "Does this thing work?" :: "Yes" :: Nil)
+    assert(before.renderFrom === before.cmd.expecteds)
+    assert(after.renderFrom === after.results)
   }
 
-  "We can paint a single After" in {
-    val cmd = SingleRowCmd(false)
-    val test = (Before(cmd) :: Nil).map(_.execute)
-
-    val result = new TreePainter().paint(test)
-
-    assert(result.tables.head.cells === title :: "Does this thing work?" :: "YesNo" :: Nil)
-  }
-
-  "We can paint Before trees" in {
-    val test = List(SingleRowCmd(true), SingleRowCmd(false), SingleRowCmd(true)).map(Before)
-
-    val result = new TreePainter().paint(test)
-
-    result.tables.foreach{table =>
-      assert(table.cells === title :: "Does this thing work?" :: "Yes" :: Nil)
-    }
-  }
-
-  "We can paint After trees" in {
+  "We can paint trees of Befores and Afters" in {
     val test = List(SingleRowCmd(true), SingleRowCmd(false), SingleRowCmd(true)).map(Before(_).execute)
 
     val result = new TreePainter().paint(test)
