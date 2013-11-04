cb681c455ef4ac6392d306ef0dbf3ab88b6ef57a
agmenc <chris@christomithsmbp.home>
2013-11-04 21:09:35 +0000

    The path to Bootstrap IIb: Iterator is better than Stream

--- src/main/scala/doc/jockey/runners/WebResourceCopier.scala
+++ src/main/scala/doc/jockey/runners/WebResourceCopier.scala
@@ -16,11 +16,11 @@ class WebResourceCopier(resourcesDir: String, targetDir: Path, sources: String*)
   private def url(source: String): Option[URL] = Option(getClass.getClassLoader.getResource(resourcesDir + File.separator + source))
   private def target(source: String) = tee(targetDir / source)(_.parent.toDirectory.createDirectory())
 
-  // TODO - CAS - 04/11/2013 - Switch to using byte buffers. This will only work for text files, and Stream.continually() will blow the stack for a large file
+  // TODO - CAS - 04/11/2013 - Switch to using byte buffers. This will only work for text files.
   private def copyText(is: InputStream, os: OutputStream) = {
     val printer = new java.io.PrintStream(os)
     val reader = new BufferedReader(new InputStreamReader(is))
-    Stream.continually(reader.readLine()).takeWhile(_ != null).foreach(printer.println)
+    Iterator.continually(reader.readLine()).takeWhile(_ != null).foreach(printer.println)
     printer.close()
   }
 
